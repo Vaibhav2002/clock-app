4ddde9220e87dd996aff5bc997a542bbd82179a9
Vaibhav2002 <vaibhavsam2511@gmail.com>
2020-07-17 21:04:17 +0530

    add the functionality of timer still alot of bugs to fix

--- app/src/main/java/com/example/clock/TimerFragment.java
+++ app/src/main/java/com/example/clock/TimerFragment.java
@@ -1,64 +1,111 @@
 package com.example.clock;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TimerFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.fragment.app.Fragment;
+
+import com.airbnb.lottie.LottieAnimationView;
+
 public class TimerFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TimerFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TimerFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TimerFragment newInstance(String param1, String param2) {
-        TimerFragment fragment = new TimerFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    EditText min, sec;
+    LottieAnimationView checkbox, playpause, stop;
+    TextView time;
+    long timeleftinmilli, STARTTIME;
+    boolean isrunning = false;
+    CountDownTimer countDownTimer;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_timer, container, false);
+        View view = inflater.inflate(R.layout.fragment_timer, container, false);
+        min = view.findViewById(R.id.min);
+        time = view.findViewById(R.id.timedisp);
+        sec = view.findViewById(R.id.sec);
+        checkbox = view.findViewById(R.id.checkboxbutton);
+        playpause = view.findViewById(R.id.playpausebutton);
+        stop = view.findViewById(R.id.stopbutton);
+        checkbox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int minutes = Integer.parseInt(min.getText().toString());
+                int seconds = Integer.parseInt(sec.getText().toString());
+                timeleftinmilli = (minutes * 60 + seconds) * 1000;
+                STARTTIME = timeleftinmilli;
+                updateTimer();
+                checkbox.playAnimation();
+            }
+        });
+
+        playpause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isrunning)
+                    pauseTimer();
+                else
+                    startTimer();
+            }
+        });
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopTimer();
+            }
+
+        });
+        updateTimer();
+        return view;
+    }
+
+
+    void startTimer() {
+        playpause.setSpeed(1);
+        playpause.playAnimation();
+        stop.setVisibility(View.INVISIBLE);
+        countDownTimer = new CountDownTimer(timeleftinmilli, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeleftinmilli = millisUntilFinished;
+                updateTimer();
+
+            }
+
+            @Override
+            public void onFinish() {
+                isrunning = false;
+                updateTimer();
+            }
+        }.start();
+        isrunning = true;
+    }
+
+    void pauseTimer() {
+        countDownTimer.cancel();
+        isrunning = false;
+        playpause.setSpeed(-2);
+        playpause.playAnimation();
+        stop.setVisibility(View.VISIBLE);
+    }
+
+    void stopTimer() {
+        stop.playAnimation();
+        timeleftinmilli = STARTTIME;
+        updateTimer();
+        stop.setVisibility(View.INVISIBLE);
+
+    }
+
+    void updateTimer() {
+        int min = (int) ((timeleftinmilli / 1000) / 60);
+        int sec = (int) ((timeleftinmilli / 1000) % 60);
+        String disptime = String.format("%02d:%02d", min, sec);
+        time.setText(disptime);
+
     }
 }
